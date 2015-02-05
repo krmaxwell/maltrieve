22a1a61fbd735c7310842046b49263b18ec3e6f9
Kyle Maxwell <kylem@xwell.org>
2015-02-05 16:53:03 -0600

    Remove currently unused feed

--- maltrieve.py
+++ maltrieve.py
@@ -300,7 +300,6 @@ def main():
     source_urls = {'https://zeustracker.abuse.ch/monitor.php?urlfeed=binaries': process_xml_list_desc,
                    'http://www.malwaredomainlist.com/hostslist/mdl.xml': process_xml_list_desc,
                    'http://malc0de.com/rss/': process_xml_list_desc,
-                   # 'http://www.malwareblacklist.com/mbl.xml',   # removed for now
                    'http://vxvault.siri-urz.net/URL_List.php': process_simple_list,
                    'http://urlquery.net/': process_urlquery,
                    'http://support.clean-mx.de/clean-mx/rss?scope=viruses&limit=0%2C64': process_xml_list_title,
