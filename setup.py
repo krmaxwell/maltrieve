248468dca29518e6a590ad7b1b3d643ad00bfa34
Kyle Maxwell <kylem@xwell.org>
2015-03-15 20:52:42 -0500

    Fix URL and version number

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-setup(name = 'maltrieve',
-    version = '1.0.0',
-    description = "A tool to retrieve malware directly from the source for security researchers.",
-    author = 'Kyle Maxwell',
-    author_email = 'krmaxwell@gmail.com',
-    url = 'https://github.com/technoskald/maltrieve',
-    package_dir = {'maltrieve': 'src'},
-    packages = ['maltrieve'],
-)
+setup(name='maltrieve',
+      version='0.6',
+      description="A tool to retrieve malware directly from the source for security researchers.",
+      author='Kyle Maxwell',
+      author_email='krmaxwell@gmail.com',
+      url='https://github.com/krmaxwell/maltrieve',
+      package_dir={'maltrieve': 'src'},
+      packages=['maltrieve'])
