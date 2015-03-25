f978bd8367d0cecd304b1668ca30ee8548e13579
Kyle Maxwell <kmaxwell@verisign.com>
2015-03-25 15:01:34 -0500

    Test argument parsing

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
 import maltrieve
 
 
-def test_args():
-    args = maltrieve.setup_args(['-l', 'testlog'])
+def test_basic_args():
+    args = maltrieve.setup_args(['-l', 'testlog', '-p', '127.0.0.1:8080', '-d', '/opt/'])
     assert args.logfile == 'testlog'
+    assert args.proxy == '127.0.0.1:8080'
+    assert args.dumpdir == '/opt/'
+
+
+def test_saving_args():
+    args = maltrieve.setup_args(['-v', '-x', '-c', '-s'])
+    assert args.viper
+    assert args.vxcage
+    assert args.cuckoo
+    assert args.sort_mime
