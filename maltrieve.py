6151b08ffba3142672c6df44c7a708f83cec569c
Kyle Maxwell <kmaxwell@verisign.com>
2015-03-25 18:08:01 -0500

    Initialize empty set if file doesn't exist. Testing found this!

--- maltrieve.py
+++ maltrieve.py
@@ -311,6 +311,8 @@ def load_urls(filename='urls.json'):
             logging.info('Loaded urls from {f}'.format(f=filename))
         except ValueError:
             urls = set()
+    else:
+        urls = set()
     return urls
 
 
